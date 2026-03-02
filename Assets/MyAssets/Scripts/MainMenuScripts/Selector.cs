1ed34d46433bb70f71f0ef6c7ddb58d529815187
Suvi Syrjäläinen <suvi.syrjalainen@gmail.com>
2026-03-02 18:57:37 +0200

    Kaarlolle again

--- Assets/MyAssets/Scripts/MainMenuScripts/Selector.cs
+++ Assets/MyAssets/Scripts/MainMenuScripts/Selector.cs
@@ -8,6 +8,8 @@ public class Selector : MonoBehaviour
     public Color normalColor;
     public Color highlightColor;
 
+    private int index = 0; 
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,6 +19,20 @@ public class Selector : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        if(Input.GetKeyDown(KeyCode.DownArrow)){
+            if(index < items.Length - 1){
+                index += 1;
+                Debug.Log(index);
+            }
+        }
+
+        if(Input.GetKeyDown(KeyCode.UpArrow)){
+            if(index > 0){
+                index -= 1;
+                Debug.Log(index);
+            }
+        }
         
     }
 }
