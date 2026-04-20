7d5d247433a02f3eb722785b3067034833f45181
DESKTOP-TEM8N3F\Mutti8888 <taavi.makela@gmail.com>
2026-04-20 19:33:32 +0300

    Level scroll aloitettu. Siirtymä Game1 tehty.

--- Assets/MyAssets/Scripts/PikkupelitScripts/ScrollGames.cs
+++ Assets/MyAssets/Scripts/PikkupelitScripts/ScrollGames.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class ScrollGames : MonoBehaviour
 {
@@ -6,14 +8,64 @@ public class ScrollGames : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     public FadeController fade;
+
+    private Vector3 centerPos;
+    private Vector3 rightPos;
+    private Vector3 leftPos;
+
+    public float levelWidth = 20f;
+
+    private int currentIndex = 0;
+
     void Start()
     {
         fade.fadeOut();
+
+        centerPos = new Vector3(0,0,0);
+        rightPos = new Vector3(levelWidth,0,0);
+        leftPos = new Vector3(-levelWidth,0,0);
+
+        for(int i = 0; i < levels.Length; i++)
+        {
+            if(i == 0)
+            {
+                levels[i].transform.position = centerPos;
+                levels[i].SetActive(true);             
+            }
+            else
+            {
+                //Siirretään muut tasot vasemmalle
+                levels[i].transform.position = leftPos;
+                levels[i].SetActive(false);
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            StartCoroutine(ChangeScene("Game1"));
+        }
+
+        if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            //Vaihdetaan seuraavaan peliin
+            currentIndex++;
+        }
+
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            currentIndex--;
+        }
+
+    }
+
+    private IEnumerator ChangeScene(string levelName)
+    {
+        fade.fadeIn();
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(levelName);
     }
 }
